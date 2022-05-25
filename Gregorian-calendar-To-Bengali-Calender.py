894ec1a65a7f4d2a64e20b551fb4493efc4d5193
Radium <105881578+radium-code@users.noreply.github.com>
2022-05-25 12:21:59 +0600

    Create Gregorian-calendar-To-Bengali-Calender.py

---
+++ Gregorian-calendar-To-Bengali-Calender.py
@@ -0,0 +1,32 @@
+monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+bmonthdays = [31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 29, 30]
+BanglaMonth = ["Baishakh", "Jaishtha", "Ashar", "Srabon", "Bhadro", "Ashwin", "Kartik", "Ogrohayon", "Poush",
+                   "Magh", "Falgun", "Chaitra"]
+
+d = input()
+m = input()
+y = input()
+
+
+def countLeapYears():
+    year = y
+    if (((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)):
+        return 1
+
+    else:
+        return 0
+
+
+def DaysInGivenYear(d, m, y):
+    tdays = 0
+    i = 0
+    for x in range(0, m - 10):
+        tdays = monthDays[i] + tdays
+        i += 1
+
+    tdays = tdays + d;
+    return tdays
+
+
+countLeapYears()
+
